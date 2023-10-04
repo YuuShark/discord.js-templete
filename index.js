e8a355947349835436919b4becfe5eb5405f799c
YuuShark <71966942+YuuShark@users.noreply.github.com>
2023-10-04 18:58:34 +0900

    Update index.js
    最低限のコードを記述

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// index.js用のコード
+// Replit用です！
+
+// config.jsを呼ぶやつ
+require("./config.js");
+
+const { TOKEN } = process.env; // SELECTに登録したenvのTOKEN
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js'); // 環境変数
+
+// DiscordBotが機能するために必要なデータ
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent, 
+    GatewayIntentBits.GuildMessages
+]
+                          });
+
+const prefix = 't'; // prefixの設定
+
+// 起動時に実行するready
+client.once('ready', () => {
+	console.log(`${client.user.tag}がログインしました！`);
+  client.user.setPresence({
+  activities: [{ name: `V14スタート`,  // ステータス名を表示する
+                 type: ActivityType.Playing  // ◯◯をプレイ中と表示する
+               }],
+               status: 'online'  // online,dnd,idle,offlineで変更可
+  });
+});
+
+// messageで定義する
+client.on('messageCreate', async (message) => {
+
+  // Botの連投対策（botの発言を無視する）
+  if (message.author.bot)
+    return;
+
+  // "hi"と喋ると"Hello World!"と返します
+  if (message.content === "hi") {
+    message.reply("Hello World!")
+  }
+});
+
+client.login(TOKEN); // Botの起動に必要（消さないこと！）
